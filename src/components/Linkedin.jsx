d3673e4cbba9f7a72a5678b42c4e279235389913
RusuOnisim <onyrusu@gmail.com>
2024-01-21 15:39:38 +0100

    linkedin button animation

---
+++ src/components/Linkedin.jsx
@@ -0,0 +1,39 @@
+import { motion } from "framer-motion";
+import { FaLinkedin } from "react-icons/fa";
+import PropTypes from "prop-types";
+
+const LinkedInButton = ({ isDarkMode, openLinkedInProfile }) => {
+  return (
+    <motion.section
+      className={`relative flex overflow-hidden items-center justify-center  ml-0 lg:ml-5 bg-${
+        isDarkMode ? "colordarksecond" : "navcolor"
+      } text-${
+        isDarkMode ? "colordarklogo" : "colorbrightlogo"
+      } rounded-xl mt-4 lg:mt-5 w-30 lg:w-52 h-11 lg:h-14 lg:text-2xl text-[15px] transition-transform duration-300 ease-in-out shadow-md hover:shadow-lg hover:scale-105`}
+      onClick={openLinkedInProfile}
+      whileHover={{
+        scale: 1.05,
+      }}
+    >
+      <motion.div
+        className={`bg-${
+          isDarkMode ? "colordarksecond" : "navcolor"
+        } absolute w-full left-0`}
+        animate={{ x: ["-7%", "95%", "-7%"] }}
+        transition={{ duration: 4.8, repeat: Infinity, delay: 1 }}
+      >
+        <FaLinkedin className="lg:text-[42px] text-3xl" />
+      </motion.div>
+      <motion.p className="lg:text-xl w-full flex pl-2 items-center justify-center text-[14px] tracking-[0.60rem]">
+        Linkedin
+      </motion.p>
+    </motion.section>
+  );
+};
+
+LinkedInButton.propTypes = {
+  isDarkMode: PropTypes.bool.isRequired,
+  openLinkedInProfile: PropTypes.func.isRequired,
+};
+
+export default LinkedInButton;
--- src/pages/App.jsx
+++ src/pages/App.jsx
@@ -1,10 +1,8 @@
-// App.js
 import {
   slideInAnimation,
   SlideInMotion,
   slideInFromRightAnimation,
 } from "../Animations/Slide";
-import { FaLinkedin } from "react-icons/fa6";
 import { motion } from "framer-motion";
 import darkModeSvg from "../assets/images/myphoto.svg";
 import lightModeSvg from "../assets/images/svvg.svg";
@@ -12,19 +10,21 @@ import TextSpan from "../components/TextSpan";
 import { useTheme } from "../components/ThemeContext";
 import PacmanIcon from "../components/PacmanIcon";
 import GitHubAnimation from "../Animations/GitHubAnimation";
+import LinkedInButton from "../components/Linkedin";
 function App() {
   const { isDarkMode } = useTheme();
+
   const sentence = "Front-End Developer".split("");
   const sentence2 = "Based in Belgium".split("");
   const svg = isDarkMode ? darkModeSvg : lightModeSvg;
+
   const openGitHubProfile = () => {
-    window.open('https://github.com/RusuOnisim', '_blank');
+    window.open("https://github.com/RusuOnisim", "_blank");
+  };
+  const openLinkedInProfile = () => {
+    window.open("https://www.linkedin.com/in/onisim-rusu-2a4b57290/", "_blank");
   };
 
-  // Function to open LinkedIn profile in a new tab
-  const openLinkedInProfile = () => {
-    window.open('https://www.linkedin.com/in/onisim-rusu-2a4b57290/', '_blank');
-  };
   return (
     <SlideInMotion
       initial="hidden"
@@ -39,10 +39,13 @@ function App() {
           isDarkMode ? "bg-colordarkbg" : "bg-colorbrightbg"
         } flex lg:flex-row flex-col-reverse z-0`}
       >
-     
+        {/* My Image Section */}
+
         <section className="sm:w-2/3 lg:h-full w-full h-auto flex flex-col justify-end self-end">
           <img src={svg} className="w-full overflow-hidden" alt="" />
         </section>
+
+        {/* Text Section  */}
 
         <section className="lg:w-1/2 lg:h-full pt-5 h-1/2 justify-start w-full flex flex-col lg:justify-center px-5">
           <motion.div
@@ -51,6 +54,8 @@ function App() {
             animate="visible"
             className="flex flex-col w-full h-1/2 items-start"
           >
+            {/* Front-End Title */}
+
             <span className="flex flex-row items-center justify-start">
               <h1
                 className={`2xl:text-5xl md:text-4xl lg:text-3xl text-xl ${
@@ -69,6 +74,9 @@ function App() {
                 }`}
               ></div>
             </span>
+
+            {/* Based in Belgium title */}
+
             <h2
               className={`2xl:text-7xl md:text-5xl text-3xl  pt-3 ${
                 isDarkMode ? "text-gray-300" : "text-colorbrightsecond"
@@ -80,6 +88,9 @@ function App() {
                 </TextSpan>
               ))}
             </h2>
+
+            {/* Description Section */}
+
             <div className="ml-0.5 lg:ml-1">
               <p
                 className={`text-gray-300/70 text-[14px] md:text-xl pt-4 pr-[10%] ${
@@ -91,6 +102,9 @@ function App() {
                 creating innovative and user-friendly experiences. Let&apos;s
                 build something amazing together!
               </p>
+
+              {/* Email */}
+
               <p
                 className={`lg:pt-4 pt-2 text-lg md:text-2xl text-${
                   isDarkMode ? "colordarklogo" : "colorbrightlogo"
@@ -99,28 +113,27 @@ function App() {
                 onyrusu@gmail.com
               </p>
             </div>
+
+            {/* Button Social Section */}
+
             <div className="flex flex-col lg:flex-row ">
-            <section
+              {/* Github Button */}
+
+              <section
                 className={`flex items-center justify-center py-2 px-2 bg-${
                   isDarkMode ? "colordarksecond" : "navcolor"
                 } text-${
                   isDarkMode ? "colordarklogo" : "colorbrightlogo"
-                } rounded-2xl mt-2 lg:mt-5 w-30 lg:w-52 h-10 lg:h-14 transition-transform duration-300 ease-in-out shadow-md hover:shadow-lg hover:scale-105`}
+                } rounded-xl mt-2 lg:mt-5 w-30 lg:w-52 h-10 lg:h-14 transition-transform duration-300 ease-in-out shadow-md hover:shadow-lg hover:scale-105`}
                 onClick={openGitHubProfile}
-            >
+              >
                 <PacmanIcon />
                 <GitHubAnimation />
               </section>
-              <section
-                className={`flex items-center justify-center py-2 px-3 lg:ml-5 bg-${
-                  isDarkMode ? "colordarksecond" : "navcolor"
-                } text-${
-                  isDarkMode ? "colordarklogo" : "colorbrightlogo"
-                } rounded-2xl mt-2 lg:mt-5 w-30 lg:w-52 h-11 lg:h-14 lg:text-2xl text-[15px] transition-transform duration-300 ease-in-out shadow-md hover:shadow-lg hover:scale-105`}
-                onClick={openLinkedInProfile}
-             >
-                <FaLinkedin className="lg:text-4xl ml-2 text-2xl" /> <p className="lg:text-xl pl-2 text-[14px] tracking-[0.50rem]">Linkedin</p> 
-              </section>
+
+              {/* Linkedin button */}
+
+              <LinkedInButton isDarkMode={isDarkMode} openLinkedInProfile={openLinkedInProfile} />
             </div>
           </motion.div>
         </section>
